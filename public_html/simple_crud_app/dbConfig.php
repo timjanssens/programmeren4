a089308c50c7e21191d3b6974f7e98cc0938793d
Tim Janssens <47323747+timjanssens@users.noreply.github.com>
2021-03-20 09:10:53 +0100

    testing

--- public_html/simple_crud_app/dbConfig.php
+++ public_html/simple_crud_app/dbConfig.php
@@ -3,7 +3,7 @@
 
 $host = "localhost";
 $username = "root";
-$password = "test";
+$password = "testing";
 $dbname = "test"; // will use later
 $dsn = "mysql:host=$host;dbname=$dbname"; // will use later
 $options = array(
