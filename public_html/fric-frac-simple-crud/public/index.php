3ba9ce0f455ef443ce2e6efce00a34cbe8b453ed
Tim Janssens <47323747+timjanssens@users.noreply.github.com>
2021-03-28 10:22:03 +0200

    Add format css

--- public_html/fric-frac-simple-crud/public/index.php
+++ public_html/fric-frac-simple-crud/public/index.php
@@ -7,6 +7,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Fric-frac simple CRUD</title>
+    <link rel="stylesheet" type="text/css" href="css/icon-font.css">
     <link rel="stylesheet" type="text/css" href="css/app.css">
 </head>
 
@@ -14,15 +15,28 @@
 
 <header class="page-header">
     <nav class="control-panel">
-        <a class="tile" href="#">Home</a>
+        <a class="tile" href="/index.php">
+            <span aria-hidden="true" class="icon-menu"></span>
+            <span class="screen-reader-text">Admin index</span>
+        </a>
     </nav>
     <h1 class="banner">Fric-frac</h1>
 </header>
 
 <article class="index">
-    <a class="tile" href="EventCategory/Index.php">Event Category</a>
-    <a class="tile" href="EventTopic/Index.php">Event Topic</a>
-    <a class="tile" href="Event/Index.php">Event Index</a>
+    <a class="tile" href="EventCategory/Index.php">
+        <span aria-hidden="true" class="icon-bookmark"></span>
+        <span class="screen-reader-text">Event Category</span>
+        Event Category
+    </a>
+    <a class="tile" href="EventTopic/Index.php">
+        <span aria-hidden="true" class="icon-price-tag"></span>
+        <span class="screen-reader-text">Event Topic</span>
+        Event Topic</a>
+    <a class="tile" href="Event/Index.php">
+        <span aria-hidden="true" class="icon-power"></span>
+        <span class="screen-reader-text">Event</span>
+        Event Index</a>
     <div class="tile _2x1">Informatieve tegel</div>
 </article>
 
